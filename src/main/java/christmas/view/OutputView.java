19cb0efde6cf3e75cabaed54f992bfe24097d47b
snow___shower <wonsb25@gmail.com>
2026-01-06 16:21:43 +0900

    feat: 입출력 양식 작성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,12 @@
+package christmas.view;
+
+public class OutputView {
+
+    public void welcome(){
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void outputPreview(){
+        System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+}
